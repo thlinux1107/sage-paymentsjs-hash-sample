cdc07fd4bb3a2c5d3035c8117822f6759330a076
Thomas Hagan <thlinux1107@users.noreply.github.com>
2016-10-19 22:33:14 -0400

    2nd attempt to upload the files

---
+++ Sage_PaymentsJS_Hash_Sample/Sage_PaymentsJS_Hash_Sample/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtJSONresp = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtHash = New System.Windows.Forms.TextBox()
+        Me.txtComputedHash = New System.Windows.Forms.TextBox()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        Me.txtResponse = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtJSONresp
+        '
+        Me.txtJSONresp.Location = New System.Drawing.Point(40, 41)
+        Me.txtJSONresp.Multiline = True
+        Me.txtJSONresp.Name = "txtJSONresp"
+        Me.txtJSONresp.Size = New System.Drawing.Size(312, 319)
+        Me.txtJSONresp.TabIndex = 0
+        Me.txtJSONresp.Text = resources.GetString("txtJSONresp.Text")
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "JSON Response"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 476)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Hash"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 532)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Computed Hash"
+        '
+        'txtHash
+        '
+        Me.txtHash.Location = New System.Drawing.Point(40, 492)
+        Me.txtHash.Name = "txtHash"
+        Me.txtHash.Size = New System.Drawing.Size(630, 20)
+        Me.txtHash.TabIndex = 4
+        '
+        'txtComputedHash
+        '
+        Me.txtComputedHash.Location = New System.Drawing.Point(40, 548)
+        Me.txtComputedHash.Name = "txtComputedHash"
+        Me.txtComputedHash.Size = New System.Drawing.Size(630, 20)
+        Me.txtComputedHash.TabIndex = 5
+        '
+        'btnProcess
+        '
+        Me.btnProcess.Location = New System.Drawing.Point(40, 366)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(75, 23)
+        Me.btnProcess.TabIndex = 6
+        Me.btnProcess.Text = "Process"
+        Me.btnProcess.UseVisualStyleBackColor = True
+        '
+        'txtResponse
+        '
+        Me.txtResponse.Location = New System.Drawing.Point(40, 437)
+        Me.txtResponse.Name = "txtResponse"
+        Me.txtResponse.Size = New System.Drawing.Size(630, 20)
+        Me.txtResponse.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(37, 421)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Response"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(682, 610)
+        Me.Controls.Add(Me.txtResponse)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnProcess)
+        Me.Controls.Add(Me.txtComputedHash)
+        Me.Controls.Add(Me.txtHash)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtJSONresp)
+        Me.Name = "Form1"
+        Me.Text = "Sage PaymentsJS Sample Hash"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtJSONresp As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtHash As TextBox
+    Friend WithEvents txtComputedHash As TextBox
+    Friend WithEvents btnProcess As Button
+    Friend WithEvents txtResponse As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Sage_PaymentsJS_Hash_Sample/Sage_PaymentsJS_Hash_Sample/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports Newtonsoft.Json.Linq
+Imports Newtonsoft.Json
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Form1
+    Private Sub btnProcess_Click(sender As Object, e As EventArgs) Handles btnProcess.Click
+
+        'TH - Gather Developer Key (Client Secret) and JSON Response
+        Dim sageDeveloperKey As String = "EzHUFdK3Egr4MI3x"
+        Dim jsonResponse As String = ""
+        jsonResponse = txtJSONresp.Text
+
+        'TH - Parse the JSON response to get the transaction Response and Hash
+        Dim jsonObject As JObject = JObject.Parse(jsonResponse)
+        Dim resp As String = jsonObject.Item("Response").ToString()
+        'Dim respObj As Object = JsonConvert.DeserializeObject(jsonObject.Item("Response").ToString())
+        Dim json_resp As Object = JsonConvert.SerializeObject(resp).ToString()
+        Dim hash As String = jsonObject.Item("Hash").ToString()
+
+        'TH - Display the parsed Response
+        txtResponse.Text = resp
+
+        'TH - Display the parsed Hash
+        txtHash.Text = hash
+
+        'TH - setup the parsed response to get the SHA512 HASH
+        Dim computedHash As Byte()
+        Dim computedHashString As String
+        Dim hmac As HMACSHA512 = New HMACSHA512(Encoding.ASCII.GetBytes(sageDeveloperKey))
+
+        'TH - Base64 encode the hash
+        computedHash = hmac.ComputeHash(Encoding.ASCII.GetBytes(json_resp))
+        computedHashString = Convert.ToBase64String(computedHash)
+
+        'TH - Display the computed hash of the parsed response.
+        txtComputedHash.Text = computedHashString
+
+
+
+    End Sub
+End Class
